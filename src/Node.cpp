a6a7790cee7dc2f3629baf9b1a2846757394c9a9
Emil Lock <emiloc-0@student.ltu.se>
2022-10-09 15:03:33 +0200

    Updated node to use link
    The node will now send messages over a given link.

--- src/Node.cpp
+++ src/Node.cpp
@@ -7,10 +7,15 @@ Node::Node(int ip, EventQueue* queue){
 
 void Node::send(std::string msg, double time){
     std::cout << "Sending message from ip: " << ip_addr << std::endl;
+    conn->transmit("message", ip_addr, time);
 }
 
 void Node::recv(std::string msg){
     std::cout << "Recv: " << msg << std::endl;
+}
+
+void Node::set_link(Link* l){
+    conn = l;
 }
 
 int Node::get_addr(){
--- src/Node.h
+++ src/Node.h
@@ -1,16 +1,21 @@
 #ifndef _NODE_H_
 #define _NODE_H_
 #include "EventQueue.h"
+#include "Link.h"
 #include <string>
 #include <iostream>
+
+class Link;
 
 class Node{
     private:
         int ip_addr;
         EventQueue* q;
+        Link* conn;
 
     public:
         Node(int ip, EventQueue* queue);
+        void set_link(Link* l);
         int get_addr();
         void send(std::string msg, double time); //Should take dest ip and message
         void recv(std::string msg); //Should take pointer to packet
