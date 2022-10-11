4b51b9ada5cb011ed58ec263accb82bad018ef62
Emil Lock <emiloc-0@student.ltu.se>
2022-10-11 17:08:58 +0200

    Updated the main file
    The main file now uses the network interface
    when setting up the nodes.

--- src/Run.cpp
+++ src/Run.cpp
@@ -3,11 +3,12 @@
 #include "Node.h"
 #include "Link.h"
 #include "SendEvent.h"
+#include "NetworkInterface.h"
 
 int main(){
     EventQueue q;
-    Node node1(1, &q);
-    Node node2(2, &q);
+    Node node1(new NetInterface("192.168.0.2", "255.255.255.0"), &q);
+    Node node2(new NetInterface("192.168.0.4", "255.255.255.0"), &q);
     Link link(&node1, &node2, 0.4, &q);
 
     q.add_event(new SendEvent(&node1, 0.0, "The first message sent!"));
