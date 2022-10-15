06d13e609cbb0d34d84003c9fe6611d0d6e318cc
Emil Lock <emiloc-0@student.ltu.se>
2022-10-15 16:48:08 +0200

    Updated Link to use cinnection interface
    The link now uses the new connection interface
    instead of node (this will allow for connections
    between e.g. router and node). The link also has
    a new connect and disconnect functionality, instead
    of connections being set on link creation.

--- src/Link.cpp
+++ src/Link.cpp
@@ -2,26 +2,55 @@
 
 using namespace std;
 
-Link::Link(Node* node1, Node* node2, double propagation_delay, EventQueue* queue){
-    addr1 = node1;
-    addr2 = node2;
-
-    addr1->set_link(this);
-    addr2->set_link(this);
-
+Link::Link(double propagation_delay, EventQueue* queue){
     delay = propagation_delay;
     q = queue;
 }
 
-void Link::transmit(string msg, string self, double current_time){
-    if(self == addr1->get_addr()){
+void Link::connect_link(Connection* new_conn){
+    /*
+        If conn1 is free set conn1 = new_conn
+        else if conn2 is free set conn2 = new_conn
+        else message error "Link cannot connect to more than 2 devices!"
+    */
+    if(new_conn == _conn1){
+        _conn1 = new_conn;
+    }else if(new_conn == _conn2){
+        _conn2 = new_conn;
+    }else{
+        cout << "Link cannot connect to more than 2 devices!" << endl;
+    }
+}
+
+void Link::disconnect_link(Connection* rm_conn){
+    /*
+        If rm_conn == conn1, set conn1 to NULL
+        else if rm_conn == conn2, set conn2 to NULL
+        else message error "Link is not connected to the device!"
+    */
+    if(rm_conn == _conn1){
+        _conn1 = NULL;
+    }else if(rm_conn == _conn2){
+        _conn2 = NULL;
+    }else{
+        cout << "Link cannot disconnect from unconnected device" << endl;
+    }
+}
+
+void Link::transmit(string msg, Connection* self, double current_time){
+    if(_conn1 == NULL || _conn2 == NULL){
+        cout << "Message cannot be sent over link, there needs to be two devices connected!" << endl;
+        return;
+    }
+
+    if(self == _conn1){
         //Create recv event for node2
-        cout << "Sending '" << msg << "' over link from ip " << addr1->get_addr() << " to ip " << addr2->get_addr() << endl;
-        q->add_event(new RecvEvent(addr2, current_time + delay, msg));
-    }else if(self == addr2->get_addr()){
+        cout << "Sending '" << msg << "' over link" << endl;
+        q->add_event(new RecvEvent(_conn2, current_time + delay, msg));
+    }else if(self == _conn2){
         //Create recv event for node1
-        cout << "Sending '" << msg << "' over link from ip " << addr2->get_addr() << " to ip " << addr1->get_addr() << endl;
-        q->add_event(new RecvEvent(addr1, current_time + delay, msg));
+        cout << "Sending '" << msg << "' over link" << endl;
+        q->add_event(new RecvEvent(_conn1, current_time + delay, msg));
     }else{
         cout << "Error occured on link." << endl;
     }
--- src/Link.h
+++ src/Link.h
@@ -1,24 +1,26 @@
 #ifndef _LINK_H_
 #define _LINK_H_
 #include "EventQueue.h"
-#include "Node.h"
 #include "RecvEvent.h"
+#include "ConnectionInterface.h"
+
 #include <string>
 #include <iostream>
 
-class Node;
 
 class Link{
     private:
-        Node* addr1;
-        Node* addr2;
+        Connection* _conn1;
+        Connection* _conn2;
 
         double delay;
 
         EventQueue* q;
 
     public:
-        Link(Node* node1, Node* node2, double propagation_delay, EventQueue* queue);
-        void transmit(std::string msg, std::string self, double current_time); // string self is temp (will be taken from header later)
+        Link(double propagation_delay, EventQueue* queue);
+        void connect_link(Connection* new_conn);
+        void disconnect_link(Connection* rm_conn);
+        void transmit(std::string msg, Connection* self, double current_time); // string self is temp (will be taken from header later)
 };
 #endif
