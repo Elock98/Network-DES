109e746c96094083aa3954f846e0ae8d35744741
Emil Lock <emiloc-0@student.ltu.se>
2022-10-15 14:36:40 +0200

    Removed get_addr from connection interface
    This interface is made to be used by the link
    and the link doesn't need to know the address,
    it only need to know to which device to relay
    the message (this will of course be the one
    that the message did not come from). Some
    mode of detecting this needs to be implemented
    into the link.

--- src/ConnectionInterface.h
+++ src/ConnectionInterface.h
@@ -7,7 +7,6 @@ class Connection{
     public:
         virtual void send(std::string msg, double time) = 0;
         virtual void recv(std::string msg) = 0;
-        virtual std::string get_addr() = 0; // Might want to change this later (switches should not have ip?)
 };
 
 #endif
