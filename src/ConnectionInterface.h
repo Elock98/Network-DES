0870f75d0e33ebda667b24b4f061aa0c965bca06
Emil Lock <emiloc-0@student.ltu.se>
2022-10-17 20:34:45 +0200

    Updated the connection interface
    The connection interface now uses the
    new message class instead of a simple
    string as message.

--- src/ConnectionInterface.h
+++ src/ConnectionInterface.h
@@ -1,12 +1,14 @@
 #ifndef _CONNECTION_INTERFACE_H_
 #define _CONNECTION_INTERFACE_H_
 
+#include "Message.h"
+
 #include <string>
 
 class Connection{
     public:
-        virtual void send(std::string msg, double time) = 0;
-        virtual void recv(std::string msg) = 0;
+        virtual void send(Message* msg, double time) = 0;
+        virtual void recv(Message* msg) = 0;
 };
 
 #endif
