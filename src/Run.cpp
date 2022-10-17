ed04bd8ec974da7dccb289dcda953f7e8900ac67
Emil Lock <emiloc-0@student.ltu.se>
2022-10-17 21:07:26 +0200

    Updated run
    Run is now setup to use the
    current implementation.

--- src/Run.cpp
+++ src/Run.cpp
@@ -4,16 +4,36 @@
 #include "Link.h"
 #include "SendEvent.h"
 #include "NetworkInterface.h"
+//#include "ConstantBitRateGenerator.h"
+#include "Message.h"
+#include "Router.h"
+
+
+#include <iostream>
+#include <vector>
 
 int main(){
     EventQueue q;
-    Node node1(new NetInterface("192.168.0.2", "255.255.255.0"), &q);
-    Node node2(new NetInterface("192.168.0.4", "255.255.255.0"), &q);
-    Link link(&node1, &node2, 0.4, &q);
 
-    q.add_event(new SendEvent(&node1, 0.0, "The first message sent!"));
-    q.add_event(new SendEvent(&node2, 0.3, "The second message sent!"));
-    q.add_event(new SendEvent(&node1, 1.3, "The third message sent!"));
+    Node node1(new NetInterface("192.168.0.2", "255.255.255.0"), &q);
+    Node node2(new NetInterface("192.168.1.2", "255.255.255.0"), &q);
+
+    Link link1(0.4, &q);
+    Link link2(0.4, &q);
+
+    node1.connect_link(&link1);
+    node2.connect_link(&link2);
+
+    std::vector<NetInterface*> r1_int = {new NetInterface("192.168.0.1", "255.255.255.0"),
+                                         new NetInterface("192.168.1.1", "255.255.255.0")};
+
+    Router r1(&q, r1_int);
+
+    r1.connect_to_interface(&link1, "192.168.0.1");
+
+    r1.connect_to_interface(&link2, "192.168.1.1");
+
+    q.add_event(new SendEvent(&node1, 0.0, new Message(node1.get_interface(), node2.get_interface(), "Test message 1")));
 
     SimEngine eng(&q);
     eng.run();
